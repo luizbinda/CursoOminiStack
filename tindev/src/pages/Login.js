d63f6e343719741479ee7624a5efae0b421e3b09
luizbinda <luiznandopimenta52@gmail.com>
2019-08-09 04:31:26 -0300

    mobile

---
+++ tindev/src/pages/Login.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {View, StyleSheet, Image, TextInput} from 'react-native';
+
+import logo from '../assets/logo.png';
+
+export default function Login(){
+    return (
+        <View style={styles.container}>
+            <Image source={logo} />
+
+            <TextInput
+            placeholder="Digite o seu usuario no Github"
+            placeholderTextColor="#999"
+            style={styles.input}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: '#f5f5f5',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30
+    },
+
+    input: {
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 4,
+        marginTop: 20,
+        paddingHorizontal: 15,
+    },
+});
